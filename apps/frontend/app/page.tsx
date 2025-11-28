fa7af9b5880efde4da99f6192a7516af1a9ee06f
Md. Tajuddin Molla <taj.unicart@gmail.com>
2025-11-28 23:22:30 +0600

    main -frontend repo added

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Button className="bg-primary">Click me</Button>
+    </div>
+  );
+}
