16e45fc589714a4c36696d0143ffeec7faf82c5e
Md. Tajuddin Molla <taj.unicart@gmail.com>
2025-12-01 22:25:03 +0600

    main Sign up ui added

--- apps/frontend/app/(auth)/sign-up/page.tsx
+++ apps/frontend/app/(auth)/sign-up/page.tsx
@@ -24,14 +24,27 @@ export default function SignUp() {
   return (
     <div className=" min-h-screen flex justify-center items-center bg-card">
       <div className=" shadow-regular w-[692px] h-[700px] bg-card rounded-lg flex flex-col gap-y-8 p-8">
-        <div>
-          <Image
-            className="max-h-[62px] max-w-[114px]"
-            alt="Logo"
-            src={Logo}
-            width={114}
-            height={62}
-          />
+        <div className=" flex gap-4">
+          <div className="min-w-[114px]">
+            <Image
+              className="max-h-[62px] max-w-[114px]"
+              alt="Logo"
+              src={Logo}
+              width={114}
+              height={62}
+            />
+          </div>
+          <div className="space-y-2">
+            <p className=" text-sm font-medium text-body">
+              Empowering Employers, Job Seekers & Trainers in Your Community
+            </p>
+            <p className=" text-xs leading-4  text-body">
+              Recruit Local connects employers, job seekers, and trainers in one
+              platform to build stronger local careers and businesses. Whether
+              you're hiring, seeking a job, or offering training, we help you
+              grow where you are.
+            </p>
+          </div>
         </div>
 
         <form
@@ -40,46 +53,94 @@ export default function SignUp() {
         >
           <div className="space-y-6">
             <div className="space-y-3">
-              <h4>Welcome Back !!</h4>
-              <p>Please log into our portal.</p>
+              <h4>Welcome to Recruit Local</h4>
+              <p>Sign Up to Unlock Exclusive Features</p>
             </div>
             <div className="space-y-6">
               <div className="space-y-4">
-                <InputGroup className="h-10 rounded-lg shadow-light">
-                  <InputGroupInput
-                    type="email"
-                    placeholder="Enter your email"
-                  />
-                  <InputGroupAddon className="group-has-[[data-slot=input-group-control]:focus-visible]/input-group:text-primary">
-                    <MailIcon />
-                  </InputGroupAddon>
-                </InputGroup>
-                <InputGroup className="h-10 rounded-lg shadow-light">
-                  <InputGroupInput type="password" placeholder="*********" />
-                  <InputGroupAddon className="group-has-[[data-slot=input-group-control]:focus-visible]/input-group:text-primary">
-                    <LockKeyholeOpen />
-                  </InputGroupAddon>
-                  <InputGroupAddon align="inline-end">
-                    <EyeClosed />
-                  </InputGroupAddon>
-                </InputGroup>
+                <div className=" grid grid-cols-2 gap-6">
+                  <div className="space-y-3">
+                    <Label className="text-title text-base leading-[100%]">
+                      First Name
+                    </Label>
+                    <InputGroup className="h-10 rounded-lg shadow-light">
+                      <InputGroupInput
+                        type="text"
+                        placeholder="Enter your First Name"
+                      />
+                    </InputGroup>
+                  </div>
+                  <div className="space-y-3">
+                    <Label className="text-title text-base leading-[100%]">
+                      Last Name
+                    </Label>
+                    <InputGroup className="h-10 rounded-lg shadow-light">
+                      <InputGroupInput
+                        type="text"
+                        placeholder="Enter your Last Name"
+                      />
+                    </InputGroup>
+                  </div>
+                </div>
+                <div className="space-y-3">
+                  <Label className="text-title text-base leading-[100%]">
+                    Email Address
+                  </Label>
+                  <InputGroup className="h-10 rounded-lg shadow-light">
+                    <InputGroupInput
+                      type="email"
+                      placeholder="Enter your email"
+                    />
+                  </InputGroup>
+                </div>
+                <div className="space-y-3">
+                  <Label className="text-title text-base leading-[100%]">
+                    New Password
+                  </Label>
+                  <InputGroup className="h-10 rounded-lg shadow-light">
+                    <InputGroupInput
+                      type="password"
+                      placeholder="Set a New Password"
+                    />
+
+                    <InputGroupAddon align="inline-end">
+                      <EyeClosed />
+                    </InputGroupAddon>
+                  </InputGroup>
+                </div>
+                <div className="space-y-3">
+                  <Label className="text-title text-base leading-[100%]">
+                    Confirm Password
+                  </Label>
+                  <InputGroup className="h-10 rounded-lg shadow-light">
+                    <InputGroupInput
+                      type="password"
+                      placeholder="Rewrite the password"
+                    />
+
+                    <InputGroupAddon align="inline-end">
+                      <EyeClosed />
+                    </InputGroupAddon>
+                  </InputGroup>
+                </div>
               </div>
               <div className=" flex justify-between items-center gap-4">
                 <div className="flex items-center gap-2">
                   <Checkbox
-                    id="remember_me"
+                    id="agreed"
                     className="w-4 h-4 rounded-xs shadow-none border border-border"
                   />
-                  <Label htmlFor="remember_me" className="text-title text-sm">
-                    Remember me
+                  <Label htmlFor="agreed" className="text-title text-sm">
+                    I agree to the{' '}
+                    <Link href={'/'} className="text-primary">
+                      Terms of Service
+                    </Link>{' '}
+                    and{' '}
+                    <Link href={'/'} className="text-primary">
+                      Privacy Notice
+                    </Link>
                   </Label>
                 </div>
-                <Link
-                  href={'/forget-password'}
-                  className=" text-sm text-primary"
-                >
-                  Forget Password?
-                </Link>
               </div>
             </div>
           </div>
@@ -88,13 +149,13 @@ export default function SignUp() {
               type="submit"
               className=" w-full text-base bg-primary border-primary text-white rounded-lg h-10"
             >
-              Login
+              Sign Up
             </Button>
             <div className=" flex justify-center">
               <p className="text-base text-body">
-                Don’t you have account?{' '}
-                <Link href={'/register'} className="text-primary">
-                  Create Account
+                Already In Recruit Local ?{' '}
+                <Link href={'/login'} className="text-primary">
+                  Login Here
                 </Link>{' '}
               </p>
             </div>
