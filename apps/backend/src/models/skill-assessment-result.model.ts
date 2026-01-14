ebc4eec9993f2b4b5ae7c11c79e6f1606fe93619
sayem-ims <sayem@imssystems.tech>
2026-01-14 15:42:07 +0600

    feat(skill-assessment): add skill assessment result model and service with schema definition

--- apps/backend/src/models/skill-assessment-result.model.ts
+++ apps/backend/src/models/skill-assessment-result.model.ts
@@ -5,4 +5,48 @@ import { softDeletePlugin, ISoftDeleteDoc, ISoftDeleteModel } from "./plugins/so
 import { AwsStorageTemplate, awsStorageTemplateMongooseDefinition } from "./templates/aws-storage.template";
 import { modelNames } from "./constants";
 
-export interface ISkillAssessmentResultInput {}
+export interface ISkillAssessmentResultInput {
+  skillAssessmentId: Schema.Types.ObjectId;
+  jobProfileId: Schema.Types.ObjectId;
+  score: number;
+  recommendations: string[];
+}
+
+export interface ISkillAssessmentResultDoc extends ISkillAssessmentResultInput, ISoftDeleteDoc, Document {
+  awsStorageTemplate: AwsStorageTemplate;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ISkillAssessmentResultModel
+  extends Model<ISkillAssessmentResultDoc>,
+    ISoftDeleteModel<ISkillAssessmentResultDoc>,
+    PaginateModel<ISkillAssessmentResultDoc>,
+    AggregatePaginateModel<ISkillAssessmentResultDoc> {}
+
+const SkillAssessmentResultSchema = new Schema<ISkillAssessmentResultDoc>(
+  {
+    skillAssessmentId: { type: Schema.Types.ObjectId, required: true, ref: modelNames.SKILL_ASSESSMENT },
+    jobProfileId: { type: Schema.Types.ObjectId, required: true, ref: modelNames.JOB_PROFILE },
+    score: { type: Number, required: true },
+    recommendations: { type: [String], required: true },
+    awsStorageTemplate: { type: awsStorageTemplateMongooseDefinition, required: false },
+    deleteMarker: {
+      status: { type: Boolean, default: false },
+      deletedAt: { type: Date, default: null },
+      dateScheduled: { type: Date, default: null },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+SkillAssessmentResultSchema.plugin(softDeletePlugin);
+SkillAssessmentResultSchema.plugin(mongoosePaginate);
+SkillAssessmentResultSchema.plugin(aggregatePaginate);
+
+export const SkillAssessmentResult = model<ISkillAssessmentResultDoc, ISkillAssessmentResultModel>(
+  modelNames.SKILL_ASSESSMENT_RESULT,
+  SkillAssessmentResultSchema
+);
