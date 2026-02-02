94ee52d0177c6e1d8dbabba049f8a09eaf34cd50
Md. Tajuddin Molla <taj.unicart@gmail.com>
2026-02-02 23:59:13 +0600

    authz -create job post ui updated

---
+++ apps/frontend/app/(authenticated)/(recruiter)/recruiter/job-lists/create/create-form.tsx
@@ -0,0 +1,329 @@
+'use client';
+import React, { useState } from 'react';
+import { Stepper } from './stepper';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Folder } from 'lucide-react';
+import {
+  InputGroup,
+  InputGroupInput,
+  InputGroupTextarea,
+} from '@/components/ui/input-group';
+
+const steps = [
+  { id: 1, label: 'Job Information' },
+  { id: 2, label: 'Review Job Description' },
+  { id: 3, label: 'Business Information' },
+  { id: 4, label: 'Preview' },
+];
+export default function CreateForm() {
+  const [currentStep, setCurrentStep] = useState(1);
+  return (
+    <div>
+      <Stepper steps={steps} currentStep={currentStep} className="mb-12" />
+      <div className=" space-y-spacing-4xl">
+        <div className="space-y-spacing-2xl">
+          <p className=" text-label-lg font-label-lg-strong!">
+            Banner of Job (optional)
+          </p>
+          <div className=" w-full p-spacing-4xl rounded-2xl border border-dashed border-border-gray-secondary flex flex-col justify-center items-center gap-spacing-lg">
+            <div className=" w-10 h-10 border border-others-brand-light flex justify-center items-center  bg-others-brand-brand-zero rounded-lg">
+              <Folder className="text-others-brand-dark fill-others-brand-dark " />
+            </div>
+            <div className=" space-y-spacing-xs text-label-sm text-center">
+              <p>
+                <span className=" text-text-brand-secondary font-label-sm-strong!">
+                  Drag & Drop
+                </span>{' '}
+                or Choose File to Upload
+              </p>
+              <p>Supported Format: SVG, JPG, PNG (up to 10mb)</p>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-spacing-2xl">
+          <p className=" text-label-lg font-label-lg-strong!">
+            Basic Information
+          </p>
+          <div className="grid grid-cols-2 gap-spacing-sm">
+            <div className="space-y-spacing-xs col-span-2">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Job Title
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder=" Eg . UI/UX Designer Wanted – Join Our Creative Team!"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Employ Type
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder="Full Time"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Job Title
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder=" Eg . UI/UX Designer Wanted – Join Our Creative Team!"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs col-span-2">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Location
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder="www.googleMap.com"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Working Days
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder="05"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Weekends
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder=" Saturday, Sunday"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Number of vacancy
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder="05"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Work palace
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder=" eg. onsite"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-spacing-2xl">
+          <p className=" text-label-lg font-label-lg-strong!">
+            Job Description
+          </p>
+          <div className="grid grid-cols-2 gap-spacing-sm">
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Salary
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder="Full Time"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs ">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Period
+              </Label>
+              <div className=" space-y-2">
+                <InputGroup className="h-12 rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupInput
+                    type="text"
+                    placeholder=" Eg . UI/UX Designer Wanted – Join Our Creative Team!"
+                    //   {...register('firstName')}
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs col-span-2">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                About the Role
+              </Label>
+              <div className=" space-y-2 ">
+                <InputGroup className="rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupTextarea
+                    placeholder="Write your message here..."
+                    //   {...register('firstName')}
+                    className="min-h-[136px] "
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs col-span-2">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Key Responsibility
+              </Label>
+              <div className=" space-y-2 ">
+                <InputGroup className="rounded-lg shadow-xs border-border-gray-primary">
+                  <InputGroupTextarea
+                    placeholder="Write your message here..."
+                    //   {...register('firstName')}
+                    className="min-h-[136px] "
+                  />
+                </InputGroup>
+                {/* {errors.firstName && (
+                    <p className="text-sm text-red-500">
+                      {errors.firstName.message}
+                    </p>
+                  )} */}
+              </div>
+            </div>
+            <div className="space-y-spacing-xs col-span-2">
+              <Label className=" text-label-sm font-label-sm-strong!">
+                Related Attachment
+              </Label>
+              <div className=" space-y-2 ">
+                <div className=" w-full p-spacing-4xl rounded-2xl border border-dashed border-border-gray-secondary flex flex-col justify-center items-center gap-spacing-lg">
+                  <div className=" w-10 h-10 border border-others-brand-light flex justify-center items-center  bg-others-brand-brand-zero rounded-lg">
+                    <Folder className="text-others-brand-dark fill-others-brand-dark " />
+                  </div>
+                  <div className=" space-y-spacing-xs text-label-sm text-center">
+                    <p>
+                      <span className=" text-text-brand-secondary font-label-sm-strong!">
+                        Drag & Drop
+                      </span>{' '}
+                      or Choose File to Upload
+                    </p>
+                    <p>Supported Format: SVG, JPG, PNG (up to 10mb)</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex py-spacing-2xl justify-end mt-spacing-4xl gap-spacing-sm">
+        <Button
+          variant="outline"
+          onClick={() => setCurrentStep(Math.max(1, currentStep - 1))}
+          disabled={currentStep === 1}
+          className=" border-border-gray-primary h-10 rounded-lg text-label-sm font-label-sm-strong! text-text-gray-primary"
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={() =>
+            setCurrentStep(Math.min(steps.length, currentStep + 1))
+          }
+          disabled={currentStep === steps.length}
+          className=" bg-bg-brand-solid-primary h-10 text-white! rounded-lg text-label-sm font-label-sm-strong!"
+        >
+          Continue
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ apps/frontend/app/(authenticated)/(recruiter)/recruiter/job-lists/create/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import CreateForm from './create-form';
+export default function CreateJob() {
+  return (
+    <div>
+      <div className=" py-spacing-lg px-spacing-4xl border-b border-border-gray-secondary">
+        <Breadcrumb className=" min-h-10 flex items-center">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                href="/recruiter/job-lists"
+                className=" text-label-sm font-label-sm-strong! text-text-gray-quaternary"
+              >
+                Job Listing
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className=" text-fg-gray-tertiary " />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="text-label-sm font-label-sm-strong! py-spacing-2xs px-spacing-md rounded-md bg-bg-brand-soft-primary text-text-brand-primary">
+                Create a job post
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <div className=" p-spacing-4xl">
+        <h3 className=" text-body-lg font-body-lg-strong! text-text-gray-primary">
+          Create a job post
+        </h3>
+        <div className=" py-spacing-4xl">
+          <CreateForm />
+        </div>
+      </div>
+    </div>
+  );
+}
