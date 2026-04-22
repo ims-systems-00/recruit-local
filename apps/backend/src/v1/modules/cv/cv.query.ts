518b7200b52e222cf5db6633e0b2104dbeafddf9
sayem-ims <sayem@imssystems.tech>
2026-04-22 13:31:48 +0600

    feat(cv): integrate resume population logic in CV queries and update visibility settings

--- apps/backend/src/v1/modules/cv/cv.query.ts
+++ apps/backend/src/v1/modules/cv/cv.query.ts
@@ -4,13 +4,15 @@ import { projectQuery } from "../../../common/query";
 import { ICVDoc, CV } from "../../../models";
 import { CvAbilityBuilder, CvAuthZEntity } from "@rl/authz";
 import { accessibleBy } from "@casl/mongoose";
-import { AbilityAction } from "@rl/types";
+import { AbilityAction, VISIBILITY_ENUM } from "@rl/types";
+import { modelNames } from "../../../models/constants";
 
 export const cvProjectQuery = (): PipelineStage[] => {
   const fieldsToExclude: (keyof ICVDoc | "__v")[] = ["__v"];
 
   const selectedFields = Object.keys(omit(CV.schema.paths, fieldsToExclude));
   selectedFields.push("status");
+  selectedFields.push("resume");
 
   return projectQuery(selectedFields);
 };
@@ -18,4 +20,52 @@ export const cvProjectQuery = (): PipelineStage[] => {
 export const cvRoleScopedSecurityQuery = (ability: ReturnType<CvAbilityBuilder["getAbility"]>) => {
   const query = accessibleBy(ability, AbilityAction.Read).ofType(CvAuthZEntity);
   return query;
+};
+
+export const populateCvResumeQuery = (): PipelineStage[] => {
+  const baseUrl = process.env.PUBLIC_MEDIA_BASE_URL || "";
+
+  return [
+    {
+      $lookup: {
+        from: modelNames.FILE_MEDIA,
+        let: { resumeId: "$resumeId" },
+        pipeline: [
+          {
+            $match: {
+              $expr: { $eq: ["$_id", "$$resumeId"] },
+            },
+          },
+          {
+            $addFields: {
+              src: {
+                $cond: {
+                  if: { $eq: ["$visibility", VISIBILITY_ENUM.PUBLIC] },
+                  then: { $concat: [baseUrl, "/", "$storageInformation.Key"] },
+                  else: null,
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              deleteMarker: 0,
+              __v: 0,
+              collectionName: 0,
+              collectionDocument: 0,
+              createdAt: 0,
+              updatedAt: 0,
+            },
+          },
+        ],
+        as: "resume",
+      },
+    },
+    {
+      $unwind: {
+        path: "$resume",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+  ];
 };
