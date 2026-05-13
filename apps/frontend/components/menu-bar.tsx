21ac70978dd6aa88387be87cbb096c24ace521f0
Md. Tajuddin Molla <taj.unicart@gmail.com>
2026-05-13 21:06:24 +0600

    feat: add NavLogo svg and integrate into menu bar

--- apps/frontend/components/menu-bar.tsx
+++ apps/frontend/components/menu-bar.tsx
@@ -11,50 +11,83 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet';
 import Link from 'next/link';
+import Image from 'next/image';
+import NavLogo from '@/public/images/NavLogo.svg';
 
 export default function MenuBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
+    <header className="bg-bg-brand-solid-alt">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-[1280px] items-center justify-between px-spacing-5xl h-[80px] text-white"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
+          <Link href="/">
+            <Image
+              className="max-h-10 max-w-[280px]"
+              alt="Logo"
+              src={NavLogo}
+              width={280}
+              height={40}
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 "
           >
             <span className="sr-only">Open main menu</span>
             <Menu aria-hidden="true" className="size-6" />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          <Link href="#" className="text-sm/6 font-semibold text-gray-900">
-            Features
+          <Link
+            href="/"
+            className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+          >
+            Home
           </Link>
-          <Link href="#" className="text-sm/6 font-semibold text-gray-900">
-            Marketplace
+          <Link
+            href="/find-job"
+            className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+          >
+            Find Job
           </Link>
-          <Link href="#" className="text-sm/6 font-semibold text-gray-900">
-            Company
+          <Link
+            href="/pricing"
+            className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+          >
+            Pricing
+          </Link>
+          <Link
+            href="/blogs"
+            className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+          >
+            Blogs
+          </Link>
+          <Link
+            href="/contact"
+            className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+          >
+            Contact us
           </Link>
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link href="#" className="text-sm/6 font-semibold text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-spacing-xl">
+          <Link
+            href="#"
+            className=" text-label-sm font-label-sm-strong! text-text-gray-quinary "
+          >
+            Sign In
+          </Link>
+          <Link
+            href="#"
+            className="text-label-sm rounded-full font-label-sm-strong! flex items-center h-10 justify-center py-spacing-xl px-spacing-3xl bg-bg-brand-solid-primary"
+          >
+            Register Now/Post Job
           </Link>
         </div>
       </nav>
@@ -62,65 +95,71 @@ export default function MenuBar() {
       <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
         <SheetContent
           side="right"
-          className="w-full overflow-y-auto bg-white p-6 sm:max-w-sm"
+          className="w-full overflow-y-auto bg-bg-brand-solid-alt text-white p-6 sm:max-w-sm border-0"
         >
-          <SheetHeader className="mb-6 flex flex-row items-center justify-between space-y-0">
+          <SheetHeader className="mb-6 flex flex-row items-center justify-between space-y-0 p-0">
             <SheetTitle className="sr-only">Mobile Menu</SheetTitle>
 
-            <Link href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-
-              <img
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Company Logo"
-                className="h-8 w-auto"
+            <Link href="/">
+              <Image
+                className="max-h-10 max-w-[280px]"
+                alt="Logo"
+                src={NavLogo}
+                width={280}
+                height={40}
               />
             </Link>
-
-            <SheetClose asChild>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <X className="h-6 w-6" />
-              </button>
-            </SheetClose>
           </SheetHeader>
 
           <div className="flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               {/* Navigation */}
-              <div className="space-y-2 py-6">
+              <div className="py-6 flex flex-col gap-spacing-xl">
                 <Link
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/"
+                  className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
                 >
-                  Features
+                  Home
                 </Link>
-
                 <Link
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/find-job"
+                  className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
                 >
-                  Marketplace
+                  Find Job
                 </Link>
-
                 <Link
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/pricing"
+                  className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
                 >
-                  Company
+                  Pricing
+                </Link>
+                <Link
+                  href="/blogs"
+                  className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+                >
+                  Blogs
+                </Link>
+                <Link
+                  href="/contact"
+                  className=" text-label-sm font-label-sm-strong! hover:text-text-brand-secondary "
+                >
+                  Contact us
                 </Link>
               </div>
 
               {/* Auth */}
-              <div className="py-6">
+              <div className="py-6 flex flex-col gap-spacing-xl">
                 <Link
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className=" text-label-sm font-label-sm-strong! text-text-gray-quinary "
                 >
-                  Log in
+                  Sign In
+                </Link>
+                <Link
+                  href="#"
+                  className="text-label-sm rounded-full font-label-sm-strong! flex items-center h-10 justify-center py-spacing-xl px-spacing-3xl bg-bg-brand-solid-primary"
+                >
+                  Register Now/Post Job
                 </Link>
               </div>
             </div>
