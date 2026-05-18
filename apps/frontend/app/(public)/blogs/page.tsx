236d35a6327cd59e63a4583a0fc6b45fb19a91b9
Md. Tajuddin Molla <taj.unicart@gmail.com>
2026-05-18 14:13:21 +0600

    feat: add BlogsPage with banner and blog card components for enhanced user engagement and insights

---
+++ apps/frontend/app/(public)/blogs/page.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Banner from './sections/banner';
+
+import BlogOneImage from '@/public/images/blog-1.jpg';
+import BlogTwoImage from '@/public/images/blog-2.png';
+import BlogThreeImage from '@/public/images/blog-3.jpg';
+import BlogCard from './sections/blog-card';
+
+export default function BlogsPage() {
+  return (
+    <div>
+      <Banner />
+      <div className="max-w-[1280px] mx-auto px-spacing-5xl py-spacing-10xl space-y-spacing-8xl">
+        <div className=" flex flex-col items-center justify-center text-center gap-y-spacing-3xl">
+          <p className=" text-label-sm font-label-sm-strong! text-text-brand-secondary">
+            BLOGS & INSIGHTS
+          </p>
+          <p className=" text-heading-lg font-heading-lg-strong! text-text-gray-primary">
+            Learn What Great Recruiters Know{' '}
+          </p>
+          <p className=" text-body-md text-text-gray-secondary">
+            Expert insights, hiring tips, and career advice for recruiters and
+            job seekers
+          </p>
+        </div>
+        <div className=" space-y-spacing-4xl">
+          <div></div>
+          <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-spacing-4xl">
+            <BlogCard
+              image={BlogOneImage}
+              title="Stand Out Faster"
+              description="Learn how job seekers can improve resumes and get noticed."
+              readTime="7"
+              date="10 jan, 2026"
+              url="#"
+            />
+            <BlogCard
+              image={BlogTwoImage}
+              title="Build Better Teams"
+              description="Explore smarter ways to attract and retain top talent."
+              readTime="7"
+              date="10 jan, 2026"
+              url="#"
+            />
+            <BlogCard
+              image={BlogThreeImage}
+              title="Career Success"
+              description="Simple career advice to help professionals grow with confidence."
+              readTime="7"
+              date="10 jan, 2026"
+              url="#"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
