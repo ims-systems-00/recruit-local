72f6bb36d98103e970833f27197bda84006b0421
sayem-ims <sayem@imssystems.tech>
2026-06-25 11:26:03 +0600

    feat(education): implement DTOs and authorization for education records, enhance security and serialization

--- apps/backend/src/v1/modules/education/education.query.ts
+++ apps/backend/src/v1/modules/education/education.query.ts
@@ -1,5 +1,8 @@
 import { omit } from "lodash";
 import { PipelineStage } from "mongoose";
+import { accessibleBy } from "@casl/mongoose";
+import { AbilityAction } from "@rl/types";
+import { EducationAbilityBuilder, EducationAuthZEntity } from "@rl/authz";
 import { projectQuery } from "../../../common/query";
 import { IEducationDoc, Education } from "../../../models";
 
@@ -9,4 +12,9 @@ export const educationProjectQuery = (): PipelineStage[] => {
   const selectedFields = Object.keys(omit(Education.schema.paths, fieldsToExclude));
 
   return projectQuery(selectedFields);
+};
+
+export const educationRoleScopedSecurityQuery = (ability: ReturnType<EducationAbilityBuilder["getAbility"]>) => {
+  const query = accessibleBy(ability, AbilityAction.Read).ofType(EducationAuthZEntity);
+  return query;
 };
