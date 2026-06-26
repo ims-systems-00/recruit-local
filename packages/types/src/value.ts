95b06fe1a010d02f1fc09117790cff8ca54b37fd
sayem-ims <sayem@imssystems.tech>
2026-06-26 17:12:19 +0600

    feat(value): refactor VALUE_TYPE_ENUM and update value seeder for new types

--- packages/types/src/value.ts
+++ packages/types/src/value.ts
@@ -1,21 +1,9 @@
 export enum VALUE_TYPE_ENUM {
-  GROWTH = 'growth',
-  LEARNING = 'learning',
-  CHALLENGE = 'challenge',
-  INNOVATION = 'innovation',
-  WORK = 'work',
-  COMMUNICATE = 'communicate',
-  ORGANIZE = 'organize',
-  CONTRIBUTE = 'contribute',
-  INTERPERSONAL = 'interpersonal',
-  ETHICAL = 'ethical',
-  SOCIAL = 'social',
-  PERFORMANCE = 'performance',
-  DEVELOPMENT = 'development',
-  COLLABORATION = 'collaboration',
-  MOTIVATION = 'motivation',
-  MEANING = 'meaning',
-  FULFILMENT = 'fulfilment',
+  WORKING_STYLE = 'working-style',
+  MINDSET = 'mindset',
+  CULTURE_AND_BEHAVIOR = 'culture-and-behavior',
+  LEADERSHIP = 'leadership',
+  TEAM_DYNAMICS = 'team-dynamics',
 }
 
 export interface IValue {
