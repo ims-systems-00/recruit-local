c17c249a38bedaf065292dc8aa692c75de203ff6
sayem-ims <sayem@imssystems.tech>
2026-07-13 10:37:11 +0600

    refactor(post): streamline IPostModel interface definition

--- apps/backend/src/models/post.model.ts
+++ apps/backend/src/models/post.model.ts
@@ -23,10 +23,7 @@ export interface IPostDoc extends IPostInput, ISoftDeleteDoc {
 }
 
 interface IPostModel
-  extends Model<IPostDoc>,
-    ISoftDeleteModel<IPostDoc>,
-    PaginateModel<IPostDoc>,
-    AggregatePaginateModel<IPostDoc> {}
+  extends Model<IPostDoc>, ISoftDeleteModel<IPostDoc>, PaginateModel<IPostDoc>, AggregatePaginateModel<IPostDoc> {}
 
 const postSchema = new Schema<IPostDoc>(
   {
